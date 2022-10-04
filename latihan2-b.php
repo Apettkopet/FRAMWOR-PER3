5d685f45f6ad7b9cedfd086f65c1d9aa5a9695b4
Apettkopet <114337431+Apettkopet@users.noreply.github.com>
2022-10-04 21:47:11 +0700

    Update latihan2-b.php

--- latihan2-b.php
+++ latihan2-b.php
@@ -1,6 +1,6 @@
 <?php
 require_once("latihan2-a.php");
-$mhs2=new mahasiswa("Jamilatur","G.211.21.0088");
+$mhs2=new mahasiswa("Muhammad Hafizh Hanafi","G.211.21.0060");
 $mhs2->cetak();
 
 // // Akan terjadi error karena Contructor dalam php oop tidak bisa di override
